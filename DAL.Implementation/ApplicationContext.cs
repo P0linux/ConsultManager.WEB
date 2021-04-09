4e451d56e4a4e37f387fd43dbbb953b6d081781d
P0linux <pollykr2000@gmail.com>
2021-04-10 00:15:34 +0300

    DbContext minor fix

--- DAL.Implementation/ApplicationContext.cs
+++ DAL.Implementation/ApplicationContext.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DAL.Implementation
 {
-    class ApplicationContext: DbContext
+    class ApplicationContext: IdentityDbContext
     {
     }
 }
