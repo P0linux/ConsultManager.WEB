47276de9860aee29b37ce975102dc90b265d8629
P0linux <pollykr2000@gmail.com>
2021-04-10 18:50:50 +0300

    Add DAL Services registration

---
+++ DAL.Implementation/DALServices.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace DAL.Implementation
+{
+    public static class DALServices
+    {
+        public static IServiceCollection RegisterDALServices(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+            services.AddIdentityCore<User>(opt =>
+            {
+                opt.Password.RequiredLength = 4;
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.User.RequireUniqueEmail = true;
+            })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationContext>();
+
+            return services;
+        }
+    }
+}
