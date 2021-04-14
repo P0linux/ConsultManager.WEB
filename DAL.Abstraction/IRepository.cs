16a0f2b32a04d48e4f5f080347f5f34454ac163e
P0linux <pollykr2000@gmail.com>
2021-04-14 13:52:53 +0300

    IRepository little fix

--- DAL.Abstraction/IRepository.cs
+++ DAL.Abstraction/IRepository.cs
@@ -8,7 +8,7 @@ namespace DAL.Abstraction
 {
     public interface IRepository<TEntity> where TEntity : BaseEntity
     {
-        Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter);
+        Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter = null);
         Task<TEntity> GetByIdAsync(int id);
         Task InsertAsync(TEntity entity);
         void Update(TEntity entity);
