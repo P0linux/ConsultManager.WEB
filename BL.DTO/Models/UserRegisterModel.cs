d3b148ab95c706bce911f5dac6e92b356c322d0d
P0linux <pollykr2000@gmail.com>
2021-04-18 18:14:31 +0300

    Add UserLoginModel and UserReqisterModel

---
+++ BL.DTO/Models/UserRegisterModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BL.DTO.Models
+{
+    class UserRegisterModel
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords are not equal")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
