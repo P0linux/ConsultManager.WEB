0eb182da37625c69348a6707dfbc91569810a1f0
P0linux <pollykr2000@gmail.com>
2021-04-27 15:27:04 +0300

    Add documentation for ConsultationController.cs

--- WebAPI/Controllers/ConsultationController.cs
+++ WebAPI/Controllers/ConsultationController.cs
@@ -20,15 +20,37 @@ namespace WebAPI.Controllers
             _consultationService = consultationService;
         }
 
+        /// <summary>
+        /// Gets all Consultation items.
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">Success</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<ConsultationDTO>>> GetAll()
             => Ok(await _consultationService.GetAllAsync());
 
+        /// <summary>
+        /// Gets a specific Consultation item by id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <response code="200">Success</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<ConsultationDTO>> GetById(int id)
-            => Ok(await _consultationService.GetByIdAsync(id)); 
+            => Ok(await _consultationService.GetByIdAsync(id));
 
+        /// <summary>
+        /// Creates a specific Consultation item.
+        /// </summary>
+        /// <param name="consultation"></param>
+        /// <returns></returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">The item is null</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Add([FromBody] ConsultationDTO consultation)
         {
             try
@@ -43,7 +65,16 @@ namespace WebAPI.Controllers
             return CreatedAtAction(nameof(Add), new { consultation.Id }, consultation);
         }
 
+        /// <summary>
+        /// Updates a specific Consultation item.
+        /// </summary>
+        /// <param name="consultation"></param>
+        /// <returns></returns>
+        /// <response code="200">Success</response>
+        /// <response code="400">The item is null</response>
         [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Update([FromBody]ConsultationDTO consultation)
         {
             if (consultation is null)
@@ -54,7 +85,14 @@ namespace WebAPI.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Deletes a specific Consultation item.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <response code="200">Success</response>
         [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Delete(int id)
         {
             await _consultationService.DeleteByIdAsync(id);
