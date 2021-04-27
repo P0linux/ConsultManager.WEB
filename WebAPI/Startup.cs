8e029c21f7654679d5a27e1018b321cced4a91f7
P0linux <pollykr2000@gmail.com>
2021-04-27 13:36:26 +0300

    Configure xml documentation for API

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -10,10 +10,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -65,7 +68,20 @@ namespace WebAPI
                     };
                 });
 
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Consultation Manager Web API",
+                    Description = "A simple example ASP.NET Core Web API",
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath); 
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -87,6 +103,7 @@ namespace WebAPI
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "ConsultationManager");
+                c.RoutePrefix = string.Empty;
             });
 
             app.UseEndpoints(endpoints =>
