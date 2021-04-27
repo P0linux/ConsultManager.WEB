15b03b395350b58fd88d6f9ae4a7345e099edf0d
P0linux <pollykr2000@gmail.com>
2021-04-27 15:04:47 +0300

    Add documentation for QueueController.cs

--- WebAPI/Controllers/QueueController.cs
+++ WebAPI/Controllers/QueueController.cs
@@ -20,15 +20,37 @@ namespace WebAPI.Controllers
             _queueService = queueService;
         }
 
+        /// <summary>
+        /// Gets all Queue items.
+        /// </summary>
+        /// <returns></returns>
+        /// <responce code="200">Success</responce>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<QueueDTO>>> GetAll()
             => Ok(await _queueService.GetAllAsync());
 
+        /// <summary>
+        /// Gets a specific Queue item by id. 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <responce code="200">Success</responce>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<QueueDTO>> GetById(int id)
             => Ok(await _queueService.GetByIdAsync(id));
 
+        /// <summary>
+        /// Creates a specific Queue item.
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <returns></returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">The item is null</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Add([FromBody] QueueDTO queue)
         {
             try
@@ -43,7 +65,16 @@ namespace WebAPI.Controllers
             return CreatedAtAction(nameof(Add), new { queue.Id }, queue);
         }
 
+        /// <summary>
+        /// Updates a specific Queue item.
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <returns></returns>
+        /// <response code="200">Success</response>
+        /// <response code="400">The item is null</response>
         [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Update([FromBody] QueueDTO queue)
         {
             if (queue is null)
@@ -54,7 +85,14 @@ namespace WebAPI.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Deletes a specific Queue item.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <response code="200">Success</response>
         [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Delete(int id)
         {
             await _queueService.DeleteByIdAsync(id);
