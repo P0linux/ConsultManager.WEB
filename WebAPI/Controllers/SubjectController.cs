e2292366526bf0e8df6149332831fbb172a60d29
P0linux <pollykr2000@gmail.com>
2021-04-27 14:05:17 +0300

    Add documentation for SubjectController.cs

--- WebAPI/Controllers/SubjectController.cs
+++ WebAPI/Controllers/SubjectController.cs
@@ -20,15 +20,33 @@ namespace WebAPI.Controllers
             _subjectService = subjectService;
         }
 
+        /// <summary>
+        /// Gets all Subject items.
+        /// </summary>
+        /// <response code="200">Success</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<SubjectDTO>>> GetAll()
             => Ok(await _subjectService.GetAllAsync());
 
+        /// <summary>
+        /// Gets a specific Subject item by id.
+        /// </summary>
+        /// <response code="200">Success</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<SubjectDTO>> GetById(int id)
             => Ok(await _subjectService.GetByIdAsync(id));
 
+
+        /// <summary>
+        /// Creates a specific Subject item.
+        /// </summary>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">The item is null</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Add([FromBody] SubjectDTO subject)
         {
             try
@@ -43,7 +61,14 @@ namespace WebAPI.Controllers
             return CreatedAtAction(nameof(Add), new { subject.Id }, subject);
         }
 
+        /// <summary>
+        /// Updates a specific Subject item.
+        /// </summary>
+        /// <response code="200">Success</response>
+        /// <response code="400">The item is null</response>
         [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Update([FromBody] SubjectDTO subject)
         {
             if (subject is null)
@@ -54,7 +79,12 @@ namespace WebAPI.Controllers
             return Ok();
         }
 
-        [HttpDelete]
+        /// <summary>
+        /// Deletes a specific Subject item.
+        /// </summary>
+        /// <response code="200">Success</response>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Delete(int id)
         {
             await _subjectService.DeleteByIdAsync(id);
