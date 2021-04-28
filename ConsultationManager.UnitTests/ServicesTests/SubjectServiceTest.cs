0d9d387916672ea12bcd7ab1203ed8fa9c769587
P0linux <pollykr2000@gmail.com>
2021-04-28 14:32:43 +0300

    Little fix SubjectControllerTests

---
+++ ConsultationManager.UnitTests/ServicesTests/SubjectServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using BL.Abstraction;
+using DAL.Abstraction;
+using DAL.Entities;
+using DAL.Implementation;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultationManager.UnitTests.ServicesTests
+{
+    [TestFixture]
+    class SubjectServiceTest
+    {
+        private ISubjectService _subjectService;
+        private ApplicationContext _applicationContext;
+        private IRepository<Subject> _subjectRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>()
+                .UseInMemoryDatabase("ConsultationManagerDb");
+
+
+        }
+    }
+}
