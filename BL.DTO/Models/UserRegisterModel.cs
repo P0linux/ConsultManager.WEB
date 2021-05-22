a9aff983bfc3c25dd0aa93abedd7fbb8c5278737
P0linux <pollykr2000@gmail.com>
2021-05-22 18:32:26 +0300

    Delete AllRoles fron UserRegisterModel

--- BL.DTO/Models/UserRegisterModel.cs
+++ BL.DTO/Models/UserRegisterModel.cs
@@ -26,7 +26,5 @@ namespace BL.DTO.Models
         [Required]
         [Display(Name = "User Role")]
         public string UserRole { get; set; }
-
-        public List<IdentityRole> AllRoles { get; set; }
     }
 }
