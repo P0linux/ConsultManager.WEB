1a073a19e9a4447baf3b9436e90ac67689d0a3e3
P0linux <pollykr2000@gmail.com>
2021-05-24 22:42:05 +0300

    Add priority calculations

--- BL.Implementation/Services/QueueMemberService.cs
+++ BL.Implementation/Services/QueueMemberService.cs
@@ -36,13 +36,16 @@ namespace BL.Implementation.Services
             var queueMembers = _unitOfWork.QueueMemberRepository.GetAllAsync()
                                           .Select(QueueMemberMapper.ProjectToDTO);
 
-            return await queueMembers.ToListAsync();
+            return await CalculateAllPriorities(queueMembers).AsQueryable().ToListAsync();
         }
 
         public async Task<QueueMemberDTO> GetByIdAsync(int id)
         {
-            var queueMember = await _unitOfWork.QueueMemberRepository.GetByIdAsync(id);
-            return queueMember.AdaptToDTO();
+            //var queueMember = await _unitOfWork.QueueMemberRepository.GetByIdAsync(id);
+
+            var queueMembers = await GetAllAsync();
+
+            return await queueMembers.AsQueryable().FirstOrDefaultAsync(qm => qm.Id == id);
         }
 
         public async Task<IEnumerable<QueueMemberDTO>> GetByQueueIdAsync(int id)
@@ -51,7 +54,7 @@ namespace BL.Implementation.Services
                 .GetAllAsync(qm => qm.QueueId == id)
                 .Select(QueueMemberMapper.ProjectToDTO);
 
-            return await queueMembers.ToListAsync();
+            return await CalculatePrioritiesByGroup(queueMembers).AsQueryable().ToListAsync();
         }
 
         public async Task UpdateAsync(QueueMemberDTO queueMember)
@@ -61,14 +64,37 @@ namespace BL.Implementation.Services
             await _unitOfWork.CommitAsync();
         }
 
-        public void CalculatePriority(QueueMemberDTO queueMember)
+        private IEnumerable<QueueMemberDTO> CalculatePrioritiesByGroup(IEnumerable<QueueMemberDTO> queueMembers)
         {
-            throw new NotImplementedException();
+            List<QueueMemberDTO> sortedMembers = queueMembers.OrderBy(qm => qm.TimeInterval).ToList();
+            
+            foreach (var member in sortedMembers)
+            {
+                int i = sortedMembers.IndexOf(member);
+                member.Priority = i + 1;
+            }
+
+            return sortedMembers;
+        }
+
+        private IEnumerable<QueueMemberDTO> CalculateAllPriorities(IEnumerable<QueueMemberDTO> queueMembers)
+        {
+            var queueMemberGroups = queueMembers.GroupBy(q => q.QueueId);
+
+            List<QueueMemberDTO> newQueueMembers = new List<QueueMemberDTO>();
+
+            foreach (var group in queueMemberGroups)
+            {
+                var prioritizedMembers = CalculatePrioritiesByGroup(group);
+                newQueueMembers.AddRange(prioritizedMembers);
+            }
+
+            return newQueueMembers;
         }
 
         public void MarkAbsent(QueueMemberDTO queueMember)
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
